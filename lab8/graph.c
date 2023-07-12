7fe39f880af721f48fb0debd42ad59b2a83236d3
shuwenzhao668 <133057990+shuwenzhao668@users.noreply.github.com>
2023-07-12 16:20:55 -0700

    Add files via upload

---
+++ lab8/graph.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int value;
+    struct node* next;
+};
+
+
+
+struct node* newNode(int value, struct node *next) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->value = value;
+    newNode->next = next;
+    return newNode;
+}
+
+
+void printAdjList(struct node* adjList[], int numOfNodes) {
+    printf("Adjacency List:\n");
+    for (int i = 0; i < numOfNodes; i++) {
+        printf("%d: ", i);
+        struct node* currentNode = adjList[i];
+        while (currentNode != NULL) {
+            printf("%d->", currentNode->value);
+            currentNode = currentNode->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int numOfNodes = 4;
+    struct node** adjList = (struct node**)malloc(numOfNodes*sizeof(struct node*));
+
+    //initialize adjList
+    for (int i = 0; i < numOfNodes; i++) {
+        adjList[i] = NULL;
+    }
+
+    int adjMatrix[4][4] = {
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {1, 0, 0, 0},
+        {0, 0, 1, 0}
+    };
+
+    printf("Adjacency Matrix:\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", adjMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < numOfNodes; i++) {
+        for (int j = 0; j < numOfNodes; j++) {
+            if (adjMatrix[i][j] == 1) {
+                if(adjList[i]==NULL){
+                    adjList[i] = newNode(j, NULL);
+                } else {
+                    adjList[i] = newNode(j, adjList[i]);
+                }
+
+            }
+        }
+    }
+
+    printAdjList(adjList,numOfNodes);
+
+    return 0;
+}
